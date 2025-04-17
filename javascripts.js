3c076e49aef6ecb49e5cbf16194bbf5ff608da53
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-17 13:31:17 -0700

    Rock paper Scissor
    Implemented the following functions:
    getHumanChoice(),getComputerChoice()
    playRound(humanChoice, computerChoice)

---
+++ javascripts.js
@@ -0,0 +1,37 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    let number = Math.random();
+    let output;
+    if (number < (1/3)){
+        output = "rock";
+    }
+    else if (number>=(1/3)&& number<(2/3)){
+        output = "paper";
+    }
+    else {
+        output = "scissors";
+    }
+    console.log("Computer has entered: "+output);
+    return output;
+}
+
+function getHumanChoice(){
+    let input = prompt("The player please enter rock/paper/scissors: ","");
+    console.log("The player has entered: " + input);
+    return input;
+}
+
+function playRound(humanChoice, computerChoice){
+    let humanChoiceLowerCase = humanChoice.toLowerCase();
+    If ()
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+// getHumanChoice();
+// getComputerChoice();
