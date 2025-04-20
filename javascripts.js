a21ad94cc5c5197acb85ff6e6504d4000fb3af12
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-19 19:26:00 -0700

    Add comments for step 1 to step 4 of the game

--- javascripts.js
+++ javascripts.js
@@ -1,11 +1,8 @@
 // This is Project: Rock Paper Scissors 
 // This game is played entirely in the console. Press F12 to get there 
 
-
-
-let humanScore = 0;
-let computerScore = 0;
-
+// getComputerChoce will randomly return one of the followings: 
+// rock, paper, scissors
 function getComputerChoice(){
     let number = Math.random();
     let output;
@@ -21,22 +18,28 @@ function getComputerChoice(){
     console.log("Computer has entered: "+output);
     return output;
 }
-
+// getHumanChoice will return one of the valid choices depending on what 
+// the user inputs. Do not need to handle reprompting if the user enters 
+// an invalid input  
 function getHumanChoice(){
     let input = prompt("The player please enter rock/paper/scissors: ","");
     console.log("The player has entered: " + input);
     return input;
 }
 
-function playRound(humanChoice, computerChoice){
-    let humanChoiceLowerCase = humanChoice.toLowerCase();
-    If ()
+// Declare the players score variables
+// Your game will keep track of the players score. 
+let humanScore = 0;
+let computerScore = 0;
 
-}
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+// function playRound(humanChoice, computerChoice){
+//     let humanChoiceLowerCase = humanChoice.toLowerCase();
+//     If ()
 
-playRound(humanSelection, computerSelection);
-// getHumanChoice();
-// getComputerChoice();
+// }
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+// playRound(humanSelection, computerSelection);
